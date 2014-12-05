4969cc4ec3320b25951063f240973762bc051191
chenleicpp <chenleicpp@qq.com>
2014-12-05 18:26:30 +0800

    添加天气数据库

---
+++ app/src/main/java/com/sanshisoft/degreeweather/AppConfig.java
@@ -0,0 +1,8 @@
+package com.sanshisoft.degreeweather;
+
+/**
+ * Created by chenleicpp on 2014/12/5.
+ */
+public class AppConfig {
+    public static final String WEATHER_ADDRESS = "http://weather.123.duba.net/static/weather_info/101090101.html";
+}
